ece3b5324ade9efe2a7f1b541087001b1144c4e1
Diego de Brito <diegosiebra13@gmail.com>
2020-04-09 12:32:10 -0400

    finished encoder-decoder architecture

--- process_glove_encoding.py
+++ process_glove_encoding.py
@@ -9,17 +9,17 @@ embedding_index = {}
 glove_path = pathlib.Path(r'C:/Users/USUARIO/glove')
 with open(glove_path / 'glove.6B.100d.txt', encoding='UTF-8') as f:
 # GCP
-#with open('./glove/glove.6B.100d.txt', encoding='UTF-8') as f:
+#with open('../glove/glove.6B.100d.txt', encoding='UTF-8') as f:
     for line in f:
         values = line.split()
         word = values[0]
         coefs = np.asarray(values[1:], dtype='float32')
         embedding_index[word] = coefs
-        
+
 # Building embedding matrix for our vocabulary
 with open("idx2word_encoder", "rb") as f:
     idx2word = pickle.load(f)
-    
+
 EMBEDDING_DIM = 100
 
 n_matched = 0
